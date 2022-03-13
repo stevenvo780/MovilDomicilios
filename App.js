d2c6df86f12503b0f1fb59cca739738c812ea30d
Stev <stevenvallejo780@gmail.com>
2022-03-12 20:22:36 -0500

    wip login

--- App.js
+++ App.js
@@ -1,12 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import { StyleSheet, Text, View } from 'react-native';
+import HomeScreen from './src/HomeScreen';
+import { AuthScreen } from './src/screens';
+import { OrdersScreen } from './src/orders';
 
+const Stack = createStackNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Home"
+            component={AuthScreen}
+          />
+          <Stack.Screen
+            name="Orders"
+            component={OrdersScreen}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
   );
 }
 
---
+++ src/HomeScreen.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+export default class HomeScreen extends React.Component {
+  render() {
+    return (
+      <View >
+        <Text>You have (undefined) Orders.</Text>
+
+        <Button
+          title="Add some orders"
+          onPress={() =>
+            this.props.navigation.navigate('Orders')
+          }
+        />
+      </View>
+    );
+  }
+}
